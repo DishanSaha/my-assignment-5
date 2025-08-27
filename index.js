79e2c79ccd7aaec521ceb5066059eb97d2749e3a
Mredul <mredulsaha1000@gmail.com>
2025-08-28 00:45:21 +0600

    my second commit add heart icon toggle in the js file

---
+++ index.js
@@ -0,0 +1,57 @@
+// heart icon toggle------------
+let count = 0;
+function putNumber(number) {
+    const loveIcon = document.getElementById('love-number');
+    loveIcon.innerText = number;
+}
+
+document.getElementById('love-icon-1').addEventListener('click', function (e) {
+    e.preventDefault();
+    count++;
+    putNumber(count);
+})
+document.getElementById('love-icon-2').addEventListener('click', function (e) {
+    e.preventDefault();
+    count++;
+    putNumber(count);
+})
+document.getElementById('love-icon-3').addEventListener('click', function (e) {
+    e.preventDefault();
+    count++;
+    putNumber(count);
+})
+document.getElementById('love-icon-4').addEventListener('click', function (e) {
+    e.preventDefault();
+    count++;
+    putNumber(count);
+})
+document.getElementById('love-icon-5').addEventListener('click', function (e) {
+    e.preventDefault();
+    count++;
+    putNumber(count);
+})
+document.getElementById('love-icon-6').addEventListener('click', function (e) {
+    e.preventDefault();
+    count++;
+    putNumber(count);
+})
+document.getElementById('love-icon-7').addEventListener('click', function (e) {
+    e.preventDefault();
+    count++;
+    putNumber(count);
+})
+document.getElementById('love-icon-8').addEventListener('click', function (e) {
+    e.preventDefault();
+    count++;
+    putNumber(count);
+})
+document.getElementById('love-icon-9').addEventListener('click', function (e) {
+    e.preventDefault();
+    count++;
+    putNumber(count);
+})
+
+
+
+
+
